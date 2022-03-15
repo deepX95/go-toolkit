ce182948e1dc2fc3096fcdc2951e1adc9abb38c5
xpc <pengcheng.xiang@daddylab.com>
2022-03-15 14:51:10 +0800

    string byte 相互转换

--- strings/strings.go
+++ strings/strings.go
@@ -1,1 +1,18 @@
 package strings
+
+import "unsafe"
+
+// StringToBytes string 转 byte数组
+func StringToBytes(str string) []byte {
+	return *(*[]byte)(unsafe.Pointer(
+		&struct {
+			string
+			Cap int
+		}{str, len(str)},
+	))
+}
+
+// BytesToString 字符数组转换为字符串，不需要创建新的内存
+func BytesToString(byte []byte) string {
+	return *(*string)(unsafe.Pointer(&byte))
+}
